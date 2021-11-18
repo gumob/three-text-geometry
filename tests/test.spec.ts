ddfb1a474023077532a959cf4b0ca81e51b8efb9
gumob <gumob.dev@gmail.com>
2021-11-18 15:57:08 +0900

    Update json file

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -4,8 +4,16 @@ import { BMFontLoader, BMFontLoaderErrorType } from '~/loader';
 import TextGeometry from '~/index';
 import { BMFont, isBMFont } from '~/types';
 
+test('BMFontLoader / XML', async () => {
+  const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular.xml';
+  // const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-pages.xml';
+  const loader = new BMFontLoader();
+  const font = await loader.load(uri);
+  expect(isBMFont(font)).toEqual(true);
+});
+
 test('BMFontLoader / Valid Json', async () => {
-  const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-msdf.json';
+  const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular.json';
   const loader = new BMFontLoader();
   const font = await loader.load(uri);
   expect(isBMFont(font)).toEqual(true);
@@ -13,7 +21,7 @@ test('BMFontLoader / Valid Json', async () => {
 
 test('BMFontLoader / Empty Json', async () => {
   try {
-    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-msdf-empty.json';
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-empty.json';
     const loader = new BMFontLoader();
     await loader.load(uri);
   } catch (error: any) {
@@ -23,7 +31,7 @@ test('BMFontLoader / Empty Json', async () => {
 
 test('BMFontLoader / Invalid Json', async () => {
   try {
-    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-msdf-invalid.json';
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-invalid.json';
     const loader = new BMFontLoader();
     await loader.load(uri);
   } catch (error: any) {
@@ -33,7 +41,7 @@ test('BMFontLoader / Invalid Json', async () => {
 
 test('BMFontLoader / Not Fouind Json', async () => {
   try {
-    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-msdf-notfound.json';
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-notfound.json';
     const loader = new BMFontLoader();
     await loader.load(uri);
   } catch (error: any) {
@@ -43,14 +51,6 @@ test('BMFontLoader / Not Fouind Json', async () => {
 
 test('BMFontLoader / Binary', async () => {
   const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Lato-Regular-16.fnt';
-  const loader = new BMFontLoader();
-  const font = await loader.load(uri);
-  expect(isBMFont(font)).toEqual(true);
-});
-
-test('BMFontLoader / XML', async () => {
-  const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular.xml';
-  // const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-pages.xml';
   const loader = new BMFontLoader();
   const font = await loader.load(uri);
   expect(isBMFont(font)).toEqual(true);
