a8bc34ab2e57926861d6dd838bf1913512024e56
gumob <gumob.dev@gmail.com>
2021-11-19 09:18:21 +0900

    Added invalid xml for unit test

--- tests/loader.spec.ts
+++ tests/loader.spec.ts
@@ -13,28 +13,63 @@ const config = {
 
 describe('BMFontLoader', () => {
 
-  test('Valid Single Page XML', async () => {
+  test('XML / Cache / Valid Single Page', async () => {
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular.xml';
+    const loader = new BMFontLoader();
+    const font = await loader.loadXML(uri);
+    expect(isBMFont(font)).toEqual(true);
+  });
+
+  test('XML / No-Cache / Valid Single Page', async () => {
     const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular.xml';
     const loader = new BMFontLoader();
     const font = await loader.loadXML(uri, config);
     expect(isBMFont(font)).toEqual(true);
   });
 
-  test('Valid Multiple Page XML', async () => {
+  test('XML / No-Cache / Valid Multiple Page', async () => {
     const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-pages.xml';
     const loader = new BMFontLoader();
     const font = await loader.loadXML(uri, config);
     expect(isBMFont(font)).toEqual(true);
   });
 
-  test('Valid Json', async () => {
+  test('XML / No-Cache / Invalid Single Page', async () => {
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-invalid.xml';
+    const loader = new BMFontLoader();
+    const font = await loader.loadXML(uri, config);
+    expect(isBMFont(font)).toEqual(true);
+  });
+
+  test('XML / No-Cache / Empty Single Page', async () => {
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-empty.xml';
+    const loader = new BMFontLoader();
+    const font = await loader.loadXML(uri, config);
+    expect(isBMFont(font)).toEqual(true);
+  });
+
+  test('XML / No-Cache / Not Found', async () => {
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-notfound.xml';
+    const loader = new BMFontLoader();
+    const font = await loader.loadXML(uri, config);
+    expect(isBMFont(font)).toEqual(true);
+  });
+
+  test('Json / Cache / Valid', async () => {
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular.json';
+    const loader = new BMFontLoader();
+    const font = await loader.loadJson(uri);
+    expect(isBMFont(font)).toEqual(true);
+  });
+
+  test('Json / No-Cache / Valid', async () => {
     const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular.json';
     const loader = new BMFontLoader();
     const font = await loader.loadJson(uri, config);
     expect(isBMFont(font)).toEqual(true);
   });
 
-  test('Empty Json', async () => {
+  test('Json / No-Cache / Empty', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-empty.json';
       const loader = new BMFontLoader();
@@ -44,7 +79,7 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Invalid Json', async () => {
+  test('Json / No-Cache / Invalid', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-invalid.json';
       const loader = new BMFontLoader();
@@ -54,7 +89,7 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Not Fouind Json', async () => {
+  test('Json / No-Cache / Not Fouind', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Roboto-Regular-notfound.json';
       const loader = new BMFontLoader();
@@ -64,7 +99,18 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Ascii / DejaVu-sdf.fnt', async () => {
+  test('Ascii / Cache / DejaVu-sdf.fnt', async () => {
+    try {
+      const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/DejaVu-sdf.fnt';
+      const loader = new BMFontLoader();
+      const font = await loader.loadAscii(uri);
+      expect(isBMFont(font)).toEqual(true);
+    } catch (error: any) {
+      console.error(error);
+    }
+  });
+
+  test('Ascii / No-Cache / DejaVu-sdf.fnt', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/DejaVu-sdf.fnt';
       const loader = new BMFontLoader();
@@ -75,7 +121,7 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Ascii / Lato-Regular-16.fnt', async () => {
+  test('Ascii / No-Cache / Lato-Regular-16.fnt', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Lato-Regular-16.fnt';
       const loader = new BMFontLoader();
@@ -86,7 +132,7 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Ascii / Lato-Regular-24.fnt', async () => {
+  test('Ascii / No-Cache / Lato-Regular-24.fnt', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Lato-Regular-24.fnt';
       const loader = new BMFontLoader();
@@ -97,7 +143,7 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Ascii / Lato-Regular-32.fnt', async () => {
+  test('Ascii / No-Cache / Lato-Regular-32.fnt', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Lato-Regular-32.fnt';
       const loader = new BMFontLoader();
@@ -108,7 +154,7 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Ascii / Lato-Regular-64.fnt', async () => {
+  test('Ascii / No-Cache / Lato-Regular-64.fnt', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Lato-Regular-64.fnt';
       const loader = new BMFontLoader();
@@ -119,7 +165,7 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Ascii / Norwester-Multi-32.fnt', async () => {
+  test('Ascii / No-Cache / Norwester-Multi-32.fnt', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Norwester-Multi-32.fnt';
       const loader = new BMFontLoader();
@@ -130,7 +176,7 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Ascii / Norwester-Multi-64.fnt', async () => {
+  test('Ascii / No-Cache / Norwester-Multi-64.fnt', async () => {
     try {
       const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Norwester-Multi-64.fnt';
       const loader = new BMFontLoader();
@@ -141,7 +187,14 @@ describe('BMFontLoader', () => {
     }
   });
 
-  test('Binary', async () => {
+  test('Binary / Cache', async () => {
+    const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Arial.bin';
+    const loader = new BMFontLoader();
+    const font = await loader.loadBinary(uri);
+    expect(isBMFont(font)).toEqual(true);
+  });
+
+  test('Binary / No-Cache', async () => {
     const uri = 'https://raw.githubusercontent.com/gumob/three-text-geometry/develop/tests/fnt/Arial.bin';
     const loader = new BMFontLoader();
     const font = await loader.loadBinary(uri, config);
