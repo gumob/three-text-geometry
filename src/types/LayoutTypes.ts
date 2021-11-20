c2073d0d83873c18cc80f35ed2ae87d4320c9909
gumob <gumob.dev@gmail.com>
2021-11-20 15:36:32 +0900

    Cleanup types

--- src/types/LayoutTypes.ts
+++ src/types/LayoutTypes.ts
@@ -28,7 +28,6 @@ enum TextLayoutAlign {
 enum WordWrapMode {
     Pre = 'pre',
     NoWrap = 'nowrap',
-    // None = 'none'
 }
 
 interface WordWrapOption {
@@ -41,94 +40,26 @@ interface WordWrapOption {
 
 interface TextLayoutOption extends WordWrapOption {
     font: BMFont | undefined;
-    // text: string | undefined;
     letterSpacing: number | undefined;
     tabSize: number | undefined;
     lineHeight: number | undefined;
     align: TextLayoutAlign | undefined;
 }
 
-interface TextGeometryOption extends TextLayoutOption {
-    flipY: boolean;
-    multipage: boolean;
-}
-
-interface WordWrapArgs {
+interface TextGeometryOption {
+    font: BMFont | undefined;
     start: number | undefined;
     end: number | undefined;
     width: number | undefined;
     mode: WordWrapMode | undefined;
-    measure: ComputeMetrics | undefined;
-}
-
-interface TextLayoutArgs extends WordWrapArgs {
-    font: BMFont | undefined;
-    text: string | undefined;
     letterSpacing: number | undefined;
     tabSize: number | undefined;
     lineHeight: number | undefined;
     align: TextLayoutAlign | undefined;
+    flipY: boolean;
+    multipage: boolean;
 }
-
-interface TextGeometryArgs extends TextLayoutArgs {
-    flipY: boolean | undefined;
-    multipage: boolean | undefined;
-}
-
-
-// function createWordWrapOption(option: any = {}): WordWrapOption {
-//     return {
-//         /** WordWrapOption specific */
-//         start: option.start !== undefined ? option.start : 0,
-//         end: option.end !== undefined ? option.end : undefined,
-//         width: option.width !== undefined ? option.width : undefined,
-//         mode: option.mode !== undefined ? option.mode : WordWrapMode.None,
-//         measure: option.measure || undefined,
-//     };
-// }
-
-// function createTextLayoutOption(option: any = {}): TextLayoutOption {
-//     // const opt = createWordWrapOption();
-//     return {
-//         /** WordWrapOption specific */
-//         start: option.start !== undefined ? option.start : 0,
-//         end: option.end !== undefined ? option.end : undefined,
-//         width: option.width !== undefined ? option.width : undefined,
-//         mode: option.mode !== undefined ? option.mode : WordWrapMode.None,
-//         measure: option.measure || undefined,
-//         /** TextLayoutOption specific */
-//         font: option.font,
-//         text: option.text,
-//         letterSpacing: option.letterSpacing !== undefined ? option.letterSpacing : 0,
-//         tabSize: option.tabSize !== undefined ? option.tabSize : 4,
-//         lineHeight: option.lineHeight,
-//         align: option.align || TextLayoutAlign.Left
-//     };
-// }
-
-// function createTextGeometryOption(option: any = {}): TextGeometryOption {
-//     return {
-//         /** WordWrapOption specific */
-//         start: option.start !== undefined ? option.start : 0,
-//         end: option.end !== undefined ? option.end : undefined,
-//         width: option.width !== undefined ? option.width : undefined,
-//         mode: option.mode !== undefined ? option.mode : WordWrapMode.None,
-//         measure: option.measure || undefined,
-//         /** TextLayoutOption specific */
-//         font: option.font,
-//         text: option.text,
-//         letterSpacing: option.letterSpacing !== undefined ? option.letterSpacing : 0,
-//         tabSize: option.tabSize !== undefined ? option.tabSize : 4,
-//         lineHeight: option.lineHeight,
-//         align: option.align || TextLayoutAlign.Left,
-//         /** TextGeometryOption specific */
-//         flipY: option.flipY !== undefined ? option.flipY : true,
-//         multipage: option.multipage !== undefined ? option.multipage : false
-//     };
-// }
 
 export {
     ComputeMetrics, TextLayoutAlign, TextLayoutOption, TextGeometryOption, TextGlyph, WordMetrics, WordWrapMode, WordWrapOption,
-    // WordWrapArgs, TextLayoutArgs, TextGeometryArgs,
-    // createWordWrapOption, createTextLayoutOption, createTextGeometryOption
 }
