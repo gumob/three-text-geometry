8afa3d6ca49886e19db302abe620501157e258c8
gumob <gumob.dev@gmail.com>
2021-11-21 16:42:44 +0900

    Cleanup

--- src/utils/binary.ts
+++ src/utils/binary.ts
@@ -15,16 +15,26 @@ function dtype(
   | Uint8ClampedArrayConstructor
   | undefined {
   switch (dtype) {
-    case 'int8': return Int8Array
-    case 'int16': return Int16Array
-    case 'int32': return Int32Array
-    case 'uint8': return Uint8Array
-    case 'uint16': return Uint16Array
-    case 'uint32': return Uint32Array
-    case 'float32': return Float32Array
-    case 'float64': return Float64Array
-    case 'uint8_clamped': return Uint8ClampedArray
-    case 'array': return Array
+    case 'int8':
+      return Int8Array
+    case 'int16':
+      return Int16Array
+    case 'int32':
+      return Int32Array
+    case 'uint8':
+      return Uint8Array
+    case 'uint16':
+      return Uint16Array
+    case 'uint32':
+      return Uint32Array
+    case 'float32':
+      return Float32Array
+    case 'float64':
+      return Float64Array
+    case 'uint8_clamped':
+      return Uint8ClampedArray
+    case 'array':
+      return Array
   }
   return undefined
 }
