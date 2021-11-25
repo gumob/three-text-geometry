d991cbd35aec0cd9f43d0e3adf004cc510538c1c
gumob <gumob.dev@gmail.com>
2021-11-25 13:23:39 +0900

    Update dist

--- dist-cjs/index.d.ts
+++ dist-cjs/index.d.ts
@@ -1,6 +1,6 @@
 import { BMFontLoader } from './loader';
 import TextGeometry from './TextGeometry';
 import { BMFont, BMFontChar, BMFontCommon, BMFontDistanceField, BMFontInfo, BMFontKern, TextAlign, WordWrapMode } from './types';
-export { BMFont, BMFontChar, BMFontCommon, BMFontDistanceField, BMFontInfo, BMFontKern, BMFontLoader, TextAlign, WordWrapMode };
+export { BMFont, BMFontChar, BMFontCommon, BMFontDistanceField, BMFontInfo, BMFontKern, BMFontLoader, TextAlign, WordWrapMode, };
 export default TextGeometry;
 //# sourceMappingURL=index.d.ts.map
