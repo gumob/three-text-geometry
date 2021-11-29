0bc3c05467a32d5a57ed55ff756c2f5226b37bcd
gumob <gumob.dev@gmail.com>
2021-11-29 13:46:08 +0900

    Replace forEach with for loop for faster computation

--- src/utils/vertices.ts
+++ src/utils/vertices.ts
@@ -2,21 +2,23 @@ import { TextGlyph } from '~/types'
 
 function extractPages(glyphs: TextGlyph[]): Float32Array {
   const pages = new Float32Array(glyphs.length * 4 * 1)
-  let i = 0
-  glyphs.forEach(function (glyph) {
+  let idx = 0
+  for (let i = 0, len = glyphs.length; i < len; i++) {
+    const glyph: TextGlyph = glyphs[i]!
     const id = glyph.data.page || 0
-    pages[i++] = id
-    pages[i++] = id
-    pages[i++] = id
-    pages[i++] = id
-  })
+    pages[idx++] = id
+    pages[idx++] = id
+    pages[idx++] = id
+    pages[idx++] = id
+  }
   return pages
 }
 
 function extractUVs(glyphs: TextGlyph[], texWidth: number, texHeight: number, flipY: boolean): Float32Array {
   const uvs = new Float32Array(glyphs.length * 4 * 2)
-  let i = 0
-  glyphs.forEach((glyph: TextGlyph) => {
+  let idx = 0
+  for (let i = 0, len = glyphs.length; i < len; i++) {
+    const glyph: TextGlyph = glyphs[i]!
     const bitmap = glyph.data
     const bw = bitmap.x + bitmap.width
     const bh = bitmap.y + bitmap.height
@@ -33,25 +35,26 @@ function extractUVs(glyphs: TextGlyph[], texWidth: number, texHeight: number, fl
     }
 
     /** BL */
-    uvs[i++] = u0
-    uvs[i++] = v1
+    uvs[idx++] = u0
+    uvs[idx++] = v1
     /** TL */
-    uvs[i++] = u0
-    uvs[i++] = v0
+    uvs[idx++] = u0
+    uvs[idx++] = v0
     /** TR */
-    uvs[i++] = u1
-    uvs[i++] = v0
+    uvs[idx++] = u1
+    uvs[idx++] = v0
     /** BR */
-    uvs[i++] = u1
-    uvs[i++] = v1
-  })
+    uvs[idx++] = u1
+    uvs[idx++] = v1
+  }
   return uvs
 }
 
 function extractPositions(glyphs: TextGlyph[]): Float32Array {
   const positions = new Float32Array(glyphs.length * 4 * 2)
-  let i = 0
-  glyphs.forEach((glyph: TextGlyph) => {
+  let idx = 0
+  for (let i = 0, len = glyphs.length; i < len; i++) {
+    const glyph: TextGlyph = glyphs[i]!
     const bitmap = glyph.data
 
     /** bottom left position */
@@ -63,18 +66,18 @@ function extractPositions(glyphs: TextGlyph[]): Float32Array {
     const h = bitmap.height
 
     /** BL */
-    positions[i++] = x
-    positions[i++] = y
+    positions[idx++] = x
+    positions[idx++] = y
     /** TL */
-    positions[i++] = x
-    positions[i++] = y + h
+    positions[idx++] = x
+    positions[idx++] = y + h
     /** TR */
-    positions[i++] = x + w
-    positions[i++] = y + h
+    positions[idx++] = x + w
+    positions[idx++] = y + h
     /** BR */
-    positions[i++] = x + w
-    positions[i++] = y
-  })
+    positions[idx++] = x + w
+    positions[idx++] = y
+  }
   return positions
 }
 
