6c74261d03d12dc551cc0737c90b3dbafd379d23
Kojiro Futamura <gumob.dev@gmail.com>
2024-08-30 00:25:55 +0900

    build: Remove .eslintrc and .eslintignore files, add eslint configuration in eslint.config.mjs to support TypeScript and disable unnecessary rules for unused variables, this-alias, case declarations, non-null assertions, and explicit any types.
    This change is intended to improve code maintainability and readability by disabling some ESLint rules that are not necessary or useful in the current project setup.

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsdoc from 'eslint-plugin-tsdoc';
+import "eslint-plugin-only-warn";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      "tsdoc": tsdoc,
+    },
+    rules: {
+      "no-unused-vars": "off",                             // Disable the rule for unused variables
+      "no-this-alias": "off",                              // Disable the rule for aliasing 'this'
+      "no-case-declarations": "off",                       // Disable the rule for case declarations
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          "varsIgnorePattern": "^_",
+          "argsIgnorePattern": "^_",
+        },
+      ],                                                   // Disable TypeScript rule for unused variables
+      "@typescript-eslint/no-non-null-assertion": "off",   // Disable TypeScript rule for non-null assertions
+      "@typescript-eslint/no-this-alias": "off",           // Disable TypeScript rule for aliasing 'this'
+      "@typescript-eslint/no-explicit-any": "off",         // Disable TypeScript rule for using 'any' type
+      "@typescript-eslint/no-unsafe-call": "off",          // Disable TypeScript rule for unsafe calls
+      "tsdoc/syntax": "warn",                              // Enable TSDoc syntax
+    },
+    files: [
+      'src/**/*.ts',
+      'src/**/*.tsx',
+    ],
+    ignores: [
+      ".yalc",
+      ".vscode",
+      ".github",
+      "node_modules",
+      "dist-cjs",
+      "dist-esm",
+      "tests",
+      "jest.config.ts",
+      "jest.config.e2e.ts",
+      "scripts",
+      "coverage",
+      "demo",
+    ]
+  }
+);
