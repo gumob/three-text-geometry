e999534b9c50c321cc5ecd7f3b96df1a612fa81f
Kojiro Futamura <gumob.dev@gmail.com>
2024-08-30 14:32:06 +0900

    build(demo/eslint.config.mjs): Add TypeScript parser and ESLint-Plugin-Only-Warn, configure project and import resolver settings for better TypeScript support.

--- demo/eslint.config.mjs
+++ demo/eslint.config.mjs
@@ -2,11 +2,28 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import tsParser from "@typescript-eslint/parser";
+import "eslint-plugin-only-warn";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
+    languageOptions: {
+      parser: tsParser,                            // TypeScript parser for ESLint
+      parserOptions: {
+        project: "tsconfig.json",                  // Path to the TypeScript configuration file
+      },
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "tsconfig.json",                // Path to the TypeScript configuration for import resolution
+        },
+      },
+    },
     rules: {
       "no-unused-vars": 0,                         // Disable the rule for unused variables
       "no-this-alias": 0,                          // Disable the rule for aliasing 'this'
