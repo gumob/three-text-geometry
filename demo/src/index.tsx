2e07ee766f01722a99c851ac7a49d922c89b9f14
Kojiro Futamura <gumob.dev@gmail.com>
2024-08-31 09:13:14 +0900

    refactor: Refactor demo application by replacing ReactDOM.render with createRoot and StrictMode from react-dom/client package, removing unused import of reportWebVitals function.

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import reportWebVitals from './reportWebVitals'
+// import reportWebVitals from './reportWebVitals'
 import Navigation from './Navigation'
 import DemoMultipage from './DemoMultipage'
 import DemoSimple from './DemoSimple'
@@ -11,8 +11,9 @@ import DemoShuffleShader from './DemoShuffleShader'
 
 import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <StrictMode>
     <BrowserRouter>
       <Navigation />
       <Routes>
@@ -24,11 +25,10 @@ ReactDOM.render(
         <Route path="/shuffleshader" element={<DemoShuffleShader />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
