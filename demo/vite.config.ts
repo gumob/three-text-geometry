6330f9b79551c1eb05fcfe444ed36e00036db1f1
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-01 10:13:46 +0900

    build(demo/vite.config.ts): Add vite-tsconfig-paths plugin for automatic TypeScript configuration
       Previously, the TS_NODE_PROJECT environment variable was used to specify the TypeScript configuration file. Now, the vite-tsconfig-paths plugin is added to automatically detect and use the tsconfig.app.json or tsconfig.json files in the project root. This change allows for a more flexible and maintainable TypeScript setup.

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
+// const tsconfigFile = process.env.TS_NODE_PROJECT || 'tsconfig.app.json';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    // tsconfigPaths({ projects: [tsconfigFile] }),
     tsconfigPaths(),
   ],
   build: {
