dfc3f5c64a35a8b2cb5e0d97edb7dff8d1a5bda2
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-01 13:35:26 +0900

    refactor: remove unnecessary closing parenthesis at the end of the BrowserRouter component

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -26,7 +26,7 @@ root.render(
       </Routes>
     </BrowserRouter>
   </StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
