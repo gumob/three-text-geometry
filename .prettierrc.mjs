3c534a08ff50a019e763a4b70c1add1afd299148
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-02 14:44:57 +0900

    build: Enable single quote

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -26,7 +26,7 @@ const config = {
   importOrderTypeScriptVersion: "5.5.4",                                   // Specifies the TypeScript version to ensure compatibility with the parser plugins. <https://github.com/IanVS/prettier-plugin-sort-imports#importorder>
   arrowParens: "always",                                                   // Controls the use of parentheses around arrow function parameters. <https://prettier.io/docs/en/options.html#arrow-function-parentheses>
   printWidth: 300,                                                         // Specifies the line length that Prettier will wrap on. <https://prettier.io/docs/en/options.html#print-width>
-  singleQuote: false,                                                      // If true, uses single quotes instead of double quotes for strings. <https://prettier.io/docs/en/options.html#quotes>
+  singleQuote: true,                                                       // If true, uses single quotes instead of double quotes for strings. <https://prettier.io/docs/en/options.html#quotes>
   semi: true,                                                              // If true, adds a semicolon at the end of every statement. <https://prettier.io/docs/en/options.html#semicolons>
   trailingComma: "all",                                                    // Controls the printing of trailing commas wherever possible. <https://prettier.io/docs/en/options.html#trailing-commas>
   tabWidth: 2,                                                             // Specifies the number of spaces per indentation level. <https://prettier.io/docs/en/options.html#tab-width>
