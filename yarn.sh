f0afd392ae26e8ae93291b11d86c8a4f8fdce4cc
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-02 16:33:18 +0900

    feat: Update yarn.sh script to use pnpm and add support for running various tasks with fzf selection, including format fixes, lint checks, build, minify, dev server start, tests, test coverage, cleaning, pre-publish, post-publish, TOC reflection, typedoc generation, and all tasks.

--- yarn.sh
+++ yarn.sh
@@ -16,14 +16,37 @@ local setup_yarn() {
 }
 
 local option_list=(
-	"yarn cache clean && rm -rf .yarn && echo \"\" > yarn.lock && rm -rf node_modules && corepack enable && yarn set version stable && yarn -v && yarn install --inline-builds && yarn dlx @yarnpkg/sdks vscode"
+    "format-fix:           Fixes the code format"
+    "format-check:         Checks the code format"
+    "lint-fix:             Fixes the code lint"
+    "lint-check:           Checks the code lint"
+    "build:                Builds the project"
+    "build-cjs:            Builds the CommonJS project"
+    "build-esm:            Builds the ESModule project"
+    "minify:               Minifies the project"
+    "minify-cjs:           Minifies the CommonJS project"
+    "minify-esm:           Minifies the ESModule project"
+    "dev:                  Starts the development server"
+    "test:                 Runs the tests"
+    "test-coverage:        Runs the tests and generates the coverage report"
+    "clean-all:            Cleans the project"
+    "clean-jest:           Cleans the jest cache"
+    "clean-coverage:       Cleans the coverage report"
+    "clean-dist:           Cleans the dist directory"
+    "clean-cache:          Cleans the cache"
+    "clean-modules:        Cleans the modules"
+    "pre-publish-only:     Pre-publishes the project"
+    "post-publish:         Post-publishes the project"
+    "reflect-toc:          Reflects the TOC"
+    "typedoc:              Generates the typedoc"
+    "all:                  Runs all the jobs"   
 )
 
 local selected_option=$(printf "%s\n" "${option_list[@]}" | fzf --ansi --prompt="Select a job to execute > ")
 
-case "$selected_option" in
-	yarn*)                                eval $selected_option;;
-	*)                                           echo "Invalid option $selected_option" && exit 1;;
-esac
+if [[ -n "$selected_option" && "$selected_option" =~ [^[:space:]] ]]; then
+    command="pnpm $selected_option"
+    eval $command;
+fi;
 
 exit 0;
