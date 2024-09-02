b2090fc0d4330c80ce04d8086e506d3d72e7088f
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-02 17:20:28 +0900

    test: Refactor Jest E2E and local configuration files    - Remove unused imports and types from ts-jest in jest.config.e2e.ts    - Update the import of pathsToModuleNameMapper to use the new API signature    - Update the import of TypeScript in both files    - Change the type of config variable in jest.config.ts to Config.InitialOptions    - Remove unused transform and testEnvironment options from jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 import * as Fs from 'fs'
-import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
-import { pathsToModuleNameMapper } from 'ts-jest/utils'
-import * as TypeScript from 'typescript'
+import type { Config } from '@jest/types'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import TypeScript from 'typescript'
 
 const tsconfig = TypeScript.readConfigFile('tsconfig.json', (path) =>
   Fs.readFileSync(path, { encoding: 'utf-8' })
 )
 
-const config: InitialOptionsTsJest = {
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   moduleNameMapper: pathsToModuleNameMapper(tsconfig.config.compilerOptions.paths, {
     prefix: '<rootDir>',
@@ -29,6 +29,10 @@ const config: InitialOptionsTsJest = {
       // },
     },
   },
+  // transform: {
+  //   '^.+\\.(ts|tsx)$': 'ts-jest',
+  // },
+  // testEnvironment: 'node',
 }
 
 export default config
