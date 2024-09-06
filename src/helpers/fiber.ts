8a1e126fa1b0971ee2287d16a055490adf234bde
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-06 22:06:50 +0900

    refactor: restructure TextGeometryNode, TextGeometryProps and TextGeometryElements types for better readability and maintainability

--- src/helpers/fiber.ts
+++ src/helpers/fiber.ts
@@ -13,7 +13,7 @@ extend({ TextGeometry });
  * @template T - The type of the TextGeometry class.
  * @template P - The type of the props for the TextGeometry component.
  */
-export type TextGeometryNode<T, P> = Overwrite<
+type TextGeometryNode<T, P> = Overwrite<
   Node<T, P>,
   {
     text: string;
@@ -24,31 +24,9 @@ export type TextGeometryNode<T, P> = Overwrite<
   EventHandlers;
 
 /**
- * TextGeometryProps is a type that extends the BufferGeometryNode type and takes two type parameters, TextGeometry and typeof TextGeometry.
- * It is used to create a props object for the TextGeometry component.
- *
- * @template T - The type of the TextGeometry class.
- * @template P - The type of the props for the TextGeometry component.
- */
-export type TextGeometryProps = TextGeometryNode<TextGeometry, typeof TextGeometry>;
-
-/**
- * TextGeometryElements is an interface that defines the elements for the TextGeometry component.
- * It has a single property, textGeometry, which is of type TextGeometryProps.
- *
- * @interface TextGeometryElements
- */
-export interface TextGeometryElements {
-  /**
-   * TextGeometry component
-   */
-  textGeometry: TextGeometryProps;
-}
-
-/**
  * Declares a global namespace for JSX.
  * It extends the IntrinsicElements interface to include the TextGeometry component.
- * 
+ *
  * @global
  * @interface IntrinsicElements
  * @extends {TextGeometryElements}
