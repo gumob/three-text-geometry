902514fa001dc3500adb5e609e4a837789a7c3b1
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-06 21:14:39 +0900

    build: Rebuild package

--- dist-esm/TextGeometry.d.ts
+++ dist-esm/TextGeometry.d.ts
@@ -2,11 +2,15 @@ import * as THREE from 'three';
 import { TextGeometryOption, TextGlyph } from './types';
 declare class TextGeometry extends THREE.BufferGeometry {
     private _opt;
+    private _text;
     private _visibleGlyphs;
     get option(): TextGeometryOption;
+    set option(value: TextGeometryOption);
+    get text(): string;
+    set text(value: string);
     get visibleGlyphs(): TextGlyph[];
     constructor(text: string, option?: any);
-    update(text: string, option?: any): void;
+    update(text?: string, option?: any): void;
     computeBoundingSphere(): void;
     computeBoundingBox(): void;
 }
--- dist-esm/helpers/fiber.d.ts
+++ dist-esm/helpers/fiber.d.ts
@@ -1,8 +1,10 @@
 import { BufferGeometryNode, Node, Overwrite } from '@react-three/fiber';
 import { EventHandlers } from '@react-three/fiber/dist/declarations/src/core/events';
 import TextGeometry from '../TextGeometry';
+import { TextGeometryOption } from '../types';
 export type TextGeometryNode<T, P> = Overwrite<Node<T, P>, {
     text: string;
+    option: TextGeometryOption;
 }> & BufferGeometryNode<TextGeometry, typeof TextGeometry> & EventHandlers;
 export type TextGeometryProps = TextGeometryNode<TextGeometry, typeof TextGeometry>;
 export interface TextGeometryElements {
