babd61d4b9815db7db787d861350b5cfb54269ad
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-06 18:05:33 +0900

    feat(helpers/fiber.ts): Add TextGeometryNode to JSX IntrinsicElements and extend fiber namespace for TextGeometry

--- src/helpers/fiber.ts
+++ src/helpers/fiber.ts
@@ -1,8 +1,10 @@
 import { BufferGeometryNode, extend, Node, Overwrite } from '@react-three/fiber';
 import { EventHandlers } from '@react-three/fiber/dist/declarations/src/core/events';
+
 import TextGeometry from '../TextGeometry';
 import { TextGeometryOption } from '../types';
-extend({ TextGeometry })
+
+extend({ TextGeometry });
 
 /**
  * TextGeometryNode is a type that extends the Node type and takes two type parameters, TextGeometry and typeof TextGeometry.
@@ -46,13 +48,15 @@ export interface TextGeometryElements {
 /**
  * Declares a global namespace for JSX.
  * It extends the IntrinsicElements interface to include the TextGeometry component.
- *
+ * 
  * @global
  * @interface IntrinsicElements
  * @extends {TextGeometryElements}
  */
 declare global {
   namespace JSX {
-    interface IntrinsicElements extends TextGeometryElements {}
+    interface IntrinsicElements {
+      textGeometry: TextGeometryNode<TextGeometry, typeof TextGeometry>;
+    }
   }
 }
