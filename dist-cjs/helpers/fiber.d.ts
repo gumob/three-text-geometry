f684fb84282b842f02faa4ba77019a6421dacc14
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-06 22:07:32 +0900

    build: Rebuild package

--- dist-cjs/helpers/fiber.d.ts
+++ dist-cjs/helpers/fiber.d.ts
@@ -2,14 +2,10 @@ import { BufferGeometryNode, Node, Overwrite } from '@react-three/fiber';
 import { EventHandlers } from '@react-three/fiber/dist/declarations/src/core/events';
 import TextGeometry from '../TextGeometry';
 import { TextGeometryOption } from '../types';
-export type TextGeometryNode<T, P> = Overwrite<Node<T, P>, {
+type TextGeometryNode<T, P> = Overwrite<Node<T, P>, {
     text: string;
     option: TextGeometryOption;
 }> & BufferGeometryNode<TextGeometry, typeof TextGeometry> & EventHandlers;
-export type TextGeometryProps = TextGeometryNode<TextGeometry, typeof TextGeometry>;
-export interface TextGeometryElements {
-    textGeometry: TextGeometryProps;
-}
 declare global {
     namespace JSX {
         interface IntrinsicElements {
@@ -17,4 +13,5 @@ declare global {
         }
     }
 }
+export {};
 //# sourceMappingURL=fiber.d.ts.map
