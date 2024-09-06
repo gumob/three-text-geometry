45b96ad240f8a6f12cebd3bc9d6450882732b3e2
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-06 17:37:54 +0900

    feat(src/helpers/fiber.ts): add TextGeometryNode and TextGeometryProps types for the TextGeometry component using @react-three/fiber     - introduce TextGeometryElements interface to define elements for the TextGeometry component     - extend JSX IntrinsicElements interface to include TextGeometry component     - export TextGeometryNodes, TextGeometryProps, and TextGeometryElements from helpers/fiber.ts

---
+++ src/helpers/fiber.ts
@@ -0,0 +1,58 @@
+import { BufferGeometryNode, extend, Node, Overwrite } from '@react-three/fiber';
+import { EventHandlers } from '@react-three/fiber/dist/declarations/src/core/events';
+import TextGeometry from '../TextGeometry';
+import { TextGeometryOption } from '../types';
+extend({ TextGeometry })
+
+/**
+ * TextGeometryNode is a type that extends the Node type and takes two type parameters, TextGeometry and typeof TextGeometry.
+ * It is used to create a props object for the TextGeometry component.
+ *
+ * @template T - The type of the TextGeometry class.
+ * @template P - The type of the props for the TextGeometry component.
+ */
+export type TextGeometryNode<T, P> = Overwrite<
+  Node<T, P>,
+  {
+    text: string;
+    option: TextGeometryOption;
+  }
+> &
+  BufferGeometryNode<TextGeometry, typeof TextGeometry> &
+  EventHandlers;
+
+/**
+ * TextGeometryProps is a type that extends the BufferGeometryNode type and takes two type parameters, TextGeometry and typeof TextGeometry.
+ * It is used to create a props object for the TextGeometry component.
+ *
+ * @template T - The type of the TextGeometry class.
+ * @template P - The type of the props for the TextGeometry component.
+ */
+export type TextGeometryProps = TextGeometryNode<TextGeometry, typeof TextGeometry>;
+
+/**
+ * TextGeometryElements is an interface that defines the elements for the TextGeometry component.
+ * It has a single property, textGeometry, which is of type TextGeometryProps.
+ *
+ * @interface TextGeometryElements
+ */
+export interface TextGeometryElements {
+  /**
+   * TextGeometry component
+   */
+  textGeometry: TextGeometryProps;
+}
+
+/**
+ * Declares a global namespace for JSX.
+ * It extends the IntrinsicElements interface to include the TextGeometry component.
+ *
+ * @global
+ * @interface IntrinsicElements
+ * @extends {TextGeometryElements}
+ */
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends TextGeometryElements {}
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ import TextGeometry from './TextGeometry';
 import { BMFont, BMFontChar, BMFontCommon, BMFontDistanceField, BMFontInfo, BMFontKern, TextAlign, TextGeometryOption, TextGlyph, WordWrapMode } from './types';
 
 export { BMFont, BMFontAsciiParser, BMFontBinaryParser, BMFontChar, BMFontCommon, BMFontDistanceField, BMFontError, BMFontInfo, BMFontJsonParser, BMFontKern, BMFontXMLParser, MultiPageShaderMaterial, MultiPageShaderMaterialParameters, TextAlign, TextGeometryOption, TextGlyph, WordWrapMode };
+export * from './helpers/fiber';
 export default TextGeometry;
