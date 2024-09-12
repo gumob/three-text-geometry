ca6e91582e27c8eb22cc3d12be1434b33203ae45
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-12 17:20:39 +0900

    feat(helpers/fiber.ts): Add TextGeometryProps type and extend JSX IntrinsicElements with TextGeometryElements interface for better type safety and readability

--- src/helpers/fiber.ts
+++ src/helpers/fiber.ts
@@ -1,4 +1,4 @@
-import { BufferGeometryNode, extend, Node, Overwrite } from '@react-three/fiber';
+import { BufferGeometryNode, extend, Node, Overwrite, ThreeElements } from '@react-three/fiber';
 import { EventHandlers } from '@react-three/fiber/dist/declarations/src/core/events';
 
 import TextGeometry from '../TextGeometry';
@@ -24,6 +24,28 @@ type TextGeometryNode<T, P> = Overwrite<
   EventHandlers;
 
 /**
+ * TextGeometryProps is a type that extends TextGeometryNode with specific type parameters.
+ * It is used to define the props for the TextGeometry component.
+ */
+export type TextGeometryProps = TextGeometryNode<TextGeometry, typeof TextGeometry>;
+
+/**
+ * TextGeometryElements is an interface that extends the ThreeElements interface and includes the TextGeometry component.
+ *
+ * @global
+ * @interface TextGeometryElements
+ * @extends {ThreeElements}
+ */
+export interface TextGeometryElements extends ThreeElements {
+  /**
+   * The props for the TextGeometry component.
+   *
+   * @type {TextGeometryProps}
+   */
+  textGeometry: TextGeometryProps;
+}
+
+/**
  * Declares a global namespace for JSX.
  * It extends the IntrinsicElements interface to include the TextGeometry component.
  *
@@ -33,8 +55,9 @@ type TextGeometryNode<T, P> = Overwrite<
  */
 declare global {
   namespace JSX {
-    interface IntrinsicElements {
-      textGeometry: TextGeometryNode<TextGeometry, typeof TextGeometry>;
-    }
+    // interface IntrinsicElements {
+    //   textGeometry: TextGeometryProps;
+    // }
+    interface IntrinsicElements extends TextGeometryElements {}
   }
 }
