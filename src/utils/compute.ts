6217d2a88ab25317af29dde121949d7d8093072a
Kojiro Futamura <gumob.dev@gmail.com>
2024-09-12 21:12:37 +0900

    fix(utils/compute.ts): refactor compute functions to use THREE.Box2 and THREE.Sphere, improve performance by avoiding unnecessary type checks. This modification fixes bug that a computingBoundingBox value sometimes returns  inconsist value.

--- src/utils/compute.ts
+++ src/utils/compute.ts
@@ -4,25 +4,21 @@ import * as THREE from 'three';
  * The function for computing the bounding box of the given positions.
  *
  * @param {ArrayLike<number>} positions - The positions to compute the bounding box.
- * @returns {object} The bounding box.
+ * @returns {Box2} The bounding box.
  */
-function bounds(positions: ArrayLike<number>) {
+function bounds(positions: ArrayLike<number>): THREE.Box2 {
   const itemSize = 2;
-  const box = { min: [0, 0], max: [0, 0] };
-  if (!positions[0] || !positions[1]) return box;
+  const box: THREE.Box2 = new THREE.Box2();
+  if (positions[0] === undefined || positions[1] === undefined) return box;
   const count = positions.length / itemSize;
-  box.min[0] = positions[0];
-  box.min[1] = positions[1];
-  box.max[0] = positions[0];
-  box.max[1] = positions[1];
+  box.min.set(positions[0], positions[1]);
+  box.max.set(positions[0], positions[1]);
   for (let i = 0; i < count; i++) {
     const x = positions[i * itemSize + 0];
     const y = positions[i * itemSize + 1];
-    if (!x || !y) continue;
-    box.min[0] = Math.min(x, box.min[0]);
-    box.min[1] = Math.min(y, box.min[1]);
-    box.max[0] = Math.max(x, box.max[0]);
-    box.max[1] = Math.max(y, box.max[1]);
+    if (x === undefined || y === undefined) continue;
+    box.min.set(Math.min(x, box.min.x), Math.min(y, box.min.y));
+    box.max.set(Math.max(x, box.max.x), Math.max(y, box.max.y));
   }
   return box;
 }
@@ -33,26 +29,26 @@ function bounds(positions: ArrayLike<number>) {
  * @param {ArrayLike<number>} positions - The positions to compute the bounding box.
  * @param {THREE.Box3} output - The output box.
  */
-function computeBox(positions: ArrayLike<number>, output: THREE.Box3) {
+function computeBox(positions: ArrayLike<number>, output: THREE.Box3): void {
   const box = bounds(positions);
-  if (!box.min[0] || !box.min[1] || !box.max[0] || !box.max[1]) return;
-  output.min.set(box.min[0], box.min[1], 0);
-  output.max.set(box.max[0], box.max[1], 0);
+  // if (box.min.x === undefined || box.min.y === undefined || box.max.x === undefined || box.max.y === undefined) return;
+  output.min.set(box.min.x, box.min.y, 0);
+  output.max.set(box.max.x, box.max.y, 0);
 }
 
 /**
  * The function for computing the bounding sphere of the given positions.
  *
- * @param {ArrayLike<number>} positions - The positions to compute the bounding sphere.
- * @param {THREE.Sphere} output - The output sphere.
+ * @param positions - The positions to compute the bounding sphere.
+ * @param output - The output sphere.
  */
-function computeSphere(positions: ArrayLike<number>, output: THREE.Sphere) {
+function computeSphere(positions: ArrayLike<number>, output: THREE.Sphere): void {
   const box = bounds(positions);
-  if (!box.min[0] || !box.min[1] || !box.max[0] || !box.max[1]) return;
-  const minX = box.min[0];
-  const minY = box.min[1];
-  const maxX = box.max[0];
-  const maxY = box.max[1];
+  // if (box.min.x === undefined || box.min.y === undefined || box.max.x === undefined || box.max.y === undefined) return;
+  const minX = box.min.x;
+  const minY = box.min.y;
+  const maxX = box.max.x;
+  const maxY = box.max.y;
   const width = maxX - minX;
   const height = maxY - minY;
   const length = Math.sqrt(width * width + height * height);
