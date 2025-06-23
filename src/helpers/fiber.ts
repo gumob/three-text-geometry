ea3f0cacdce9881e0a667a96be9100607eb4ae85
Kojiro Futamura <gumob.dev@gmail.com>
2025-06-23 19:32:52 +0900

    refactor: Simplify TextGeometry type definitions and remove unused code for @react-three/fiber@9.1.2
    - Removed unused imports and commented-out code related to TextGeometryNode.
    - Updated TextGeometryProps to use ThreeElement instead of BufferGeometryNode for better type handling.

--- src/helpers/fiber.ts
+++ src/helpers/fiber.ts
@@ -1,36 +1,14 @@
-import { BufferGeometryNode, extend, ThreeElements } from '@react-three/fiber';
-
+import { extend, ThreeElement, ThreeElements } from '@react-three/fiber';
 import TextGeometry from '../TextGeometry';
 
 extend({ TextGeometry });
-
-// import { Node, Overwrite } from '@react-three/fiber';
-// import { EventHandlers } from '@react-three/fiber/dist/declarations/src/core/events';
-// import { TextGeometryOption } from '../types';
-
-// /**
-//  * TextGeometryNode is a type that extends the Node type and takes two type parameters, TextGeometry and typeof TextGeometry.
-//  * It is used to create a props object for the TextGeometry component.
-//  *
-//  * @template T - The type of the TextGeometry class.
-//  * @template P - The type of the props for the TextGeometry component.
-//  */
-// type TextGeometryNode<T, P> = Overwrite<
-//   Node<T, P>,
-//   {
-//     text: string;
-//     option: TextGeometryOption;
-//   }
-// > &
-//   BufferGeometryNode<TextGeometry, typeof TextGeometry> &
-//   EventHandlers;
 
 /**
  * TextGeometryProps is a type that extends TextGeometryNode with specific type parameters.
  * It is used to define the props for the TextGeometry component.
  */
 // export type TextGeometryProps = TextGeometryNode<TextGeometry, typeof TextGeometry>;
-export type TextGeometryProps = BufferGeometryNode<TextGeometry, typeof TextGeometry>;
+export type TextGeometryProps = ThreeElement<typeof TextGeometry>;
 
 /**
  * TextGeometryElements is an interface that extends the ThreeElements interface and includes the TextGeometry component.
