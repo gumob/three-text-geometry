94d35e12074e3aed5d031fdf7d8a90c83a74ae13
Kojiro Futamura <gumob.dev@gmail.com>
2025-06-25 21:15:10 +0900

    chore: update fiber source maps for consistency and accuracy
    - Modified the source maps for both CommonJS and ESM builds of the fiber helper to ensure consistency in the mappings and file references.
    - Adjusted the mappings in the JavaScript and TypeScript definition files to reflect the latest changes in the source code, enhancing debugging and development experience.

--- dist-esm/shader/MultiPageShaderMaterialParameters.js
+++ dist-esm/shader/MultiPageShaderMaterialParameters.js
@@ -1,4 +1,4 @@
-import*as THREE from"three";class MultiPageShaderMaterialParameters{constructor(e){var e=e||{},t="number"==typeof e.opacity?e.opacity:1,a=e.precision||"highp",r="number"==typeof e.alphaTest?e.alphaTest:1e-4,o=e.textures||[];let i={};o.forEach((e,t)=>{i["texture"+t]={type:"t",value:e}});var n=o.map(function(e,t){return"uniform sampler2D texture"+t+";"}).join("\n"),o=o.map(function(e,t){return[(0===t?"if":"else if")+" (vPage == "+t+".0) {","sampleColor = texture2D(texture"+t+", vUv);","}"].join("\n")}).join("\n"),l=e.color;delete e.textures,delete e.color,delete e.precision,delete e.opacity;let v={attributes:{page:{type:"f",value:0}}};72<=(0|(parseInt(THREE.REVISION,10)||0))&&(v=void 0);r=0===r?"":`if (gl_FragColor.a < ${r}) discard;`,t=Object.assign({uniforms:Object.assign({},i,{opacity:{type:"f",value:t},color:{type:"c",value:l}}),vertexShader:`
+import*as THREE from"three";class MultiPageShaderMaterialParameters{constructor(e){var t="number"==typeof(e=e||{}).opacity?e.opacity:1,a=e.precision||"highp",r="number"==typeof e.alphaTest?e.alphaTest:1e-4;let o={};(n=e.textures||[]).forEach((e,t)=>{o["texture"+t]={type:"t",value:e}});var i=n.map(function(e,t){return"uniform sampler2D texture"+t+";"}).join("\n"),n=n.map(function(e,t){return[(0===t?"if":"else if")+" (vPage == "+t+".0) {","sampleColor = texture2D(texture"+t+", vUv);","}"].join("\n")}).join("\n"),l=e.color;delete e.textures,delete e.color,delete e.precision,delete e.opacity;let v={attributes:{page:{type:"f",value:0}}};72<=(0|(parseInt(THREE.REVISION,10)||0))&&(v=void 0),r=0===r?"":`if (gl_FragColor.a < ${r}) discard;`,t=Object.assign({uniforms:Object.assign({},o,{opacity:{type:"f",value:t},color:{type:"c",value:l}}),vertexShader:`
                 attribute vec4 position;
                 attribute vec2 uv;
                 attribute float page;
@@ -15,13 +15,13 @@ import*as THREE from"three";class MultiPageShaderMaterialParameters{constructor(
                 precision ${a} float;
                 uniform float opacity;
                 uniform vec3 color;
-                ${n}
+                ${i}
                 varying float vPage;
                 varying vec2 vUv;
                 void main() {
                     vec4 sampleColor = vec4(0.0);
-                    ${o}
+                    ${n}
                     gl_FragColor = sampleColor * vec4(color, opacity);
                     ${r}
                 }
-                `},v,e);this.uniforms=t.uniforms,this.vertexShader=t.vertexShader,this.fragmentShader=t.fragmentShader}}export{MultiPageShaderMaterialParameters};
+                `},v,e),this.uniforms=t.uniforms,this.vertexShader=t.vertexShader,this.fragmentShader=t.fragmentShader}}export{MultiPageShaderMaterialParameters};
